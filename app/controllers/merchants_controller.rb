60cc4f030560f559dbd498754b9b5b4ab925be99
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-03-18 13:31:40 +0500

    Refactor merchants controller
    Apply persenters and query objects

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,14 +1,15 @@
 class MerchantsController < ApplicationController
   def index
-    @merchants = User.where(role: :merchant).page params[:page]
+    @merchants = LoadMerchants.call(list_params)
   end
 
   def edit
-    @merchant = User.find params[:id]
+    @merchant = LoadMerchant.call(item_params)
+    @transactions = LoadTransactions.call(item_params)
   end
 
   def update
-    @merchant = User.find params[:id]
+    @merchant = LoadMerchant.call(params[:id])
     if @merchant.update resource_params
       redirect_to action: :index
     else
@@ -18,7 +19,15 @@ class MerchantsController < ApplicationController
 
   private
 
+  def list_params
+    params.permit(:page)
+  end
+
+  def item_params
+    params.permit(:id)
+  end
+
   def resource_params
-    params.require(:user).permit(:email, :name, :id)
+    params.require(:user).permit(:email, :name, :id, :activity)
   end
 end
