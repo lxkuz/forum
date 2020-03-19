80d4f54d943d9dab5a1d12cbad0d6588c354e717
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-03-20 01:14:21 +0500

    Add authenticate_admin to merchants controller

--- app/controllers/merchants_controller.rb
+++ app/controllers/merchants_controller.rb
@@ -1,15 +1,18 @@
 class MerchantsController < ApplicationController
+  before_action :authenticate_user!
+  before_action :authenticate_admin!
+
   def index
-    @merchants = LoadMerchants.call(list_params)
+    @merchants = LoadMerchants.call(params)
   end
 
   def edit
-    @merchant = LoadMerchant.call(item_params)
-    @transactions = LoadTransactions.call(item_params)
+    @merchant = LoadMerchant.call(params)
+    @transactions = LoadTransactions.call(params)
   end
 
   def update
-    @merchant = LoadMerchant.call(params[:id])
+    @merchant = LoadMerchant.call(params)
     if @merchant.update resource_params
       redirect_to action: :index
     else
@@ -19,12 +22,8 @@ class MerchantsController < ApplicationController
 
   private
 
-  def list_params
-    params.permit(:page)
-  end
-
-  def item_params
-    params.permit(:id)
+  def authenticate_admin!
+    redirect_to user_session_path unless current_user.admin?
   end
 
   def resource_params
