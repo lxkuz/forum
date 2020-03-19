d8473171935f579b56cc43ee45ea2b9eb23e8d5c
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-03-20 01:23:55 +0500

    Setup devise login to controller specs

---
+++ spec/support/controller_macros.rb
@@ -0,0 +1,10 @@
+# require Rails.root.join('spec', 'factories', 'user')
+
+module ControllerMacros
+  def login_admin
+    before(:each) do
+      @request.env['devise.mapping'] = Devise.mappings[:admin]
+      sign_in FactoryBot.create(:admin)
+    end
+  end
+end
