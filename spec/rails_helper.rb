a9fb6cb62963c754559f2f0a304d0f40b089f94a
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-03-20 01:57:41 +0500

    Apply simplecov logging for rails

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -7,6 +7,8 @@ abort('The Rails environment is running in production mode!') if Rails.env.produ
 require 'rspec/rails'
 require 'devise'
 require 'factory_bot_rails'
+require 'simplecov'
+SimpleCov.start 'rails'
 
 # Add additional requires below this line. Rails is not loaded until this point!
 
