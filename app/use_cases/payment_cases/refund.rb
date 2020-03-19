eee1dc730daad146c85599ae87cf2d6335cc3a65
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-03-20 01:54:46 +0500

    Apply rubocop polushing for use_cases

--- app/use_cases/payment_cases/refund.rb
+++ app/use_cases/payment_cases/refund.rb
@@ -1,24 +1,22 @@
 module PaymentCases
   class Refund < PaymentCases::Base
     def call
-      begin
-        handle_options
-        load_charge_transaction
-        Merchant.transaction do
-          AuthorizeTransaction.transaction do
-            RefundTransaction.transaction do
-              refund_charge_transaction
-              create_refund_transaction
-              decrement_merchant_sum_of_payments
-              approve_refund_transaction
-            end
+      handle_options
+      load_charge_transaction
+      Merchant.transaction do
+        AuthorizeTransaction.transaction do
+          RefundTransaction.transaction do
+            refund_charge_transaction
+            create_refund_transaction
+            decrement_merchant_sum_of_payments
+            approve_refund_transaction
           end
         end
-      rescue UseCaseError => e
-        error_response(e.message)
-      else
-        success_response
       end
+    rescue UseCaseError => e
+      error_response(e.message)
+    else
+      success_response
     end
 
     private
@@ -27,26 +25,22 @@ module PaymentCases
 
     def handle_options
       @amount = options[:amount].to_f
-      if @amount <= 0
-        raise_error(:wrong_amount)
-      end
+      raise_error(:wrong_amount) if @amount <= 0
     end
 
     def load_charge_transaction
       @charge_transaction = Transaction.where(
-        customer_email: options[:customer_email]
+        customer_email: options[:customer_email],
         uuid: options[:uuid]
       ).reorder(:created_at).last
 
       if @charge_transaction.nil? ||
-        @charge_transaction.is_a?(ChargeTransaction) ||
-        !@charge_transaction.approved?
+         @charge_transaction.is_a?(ChargeTransaction) ||
+         !@charge_transaction.approved?
         raise_error(:no_charge_transaction)
       end
 
-      if @charge_transaction.amount == @amount   
-        raise_error(:wrong_amount)
-      end
+      raise_error(:wrong_amount) if @charge_transaction.amount == @amount
     end
 
     def refund_charge_transaction
@@ -62,9 +56,7 @@ module PaymentCases
         customer_phone: options[:customer_phone],
         customer_email: options[:customer_email]
       )
-      unless @charge_transaction.save
-        raise_error(@charge_transaction.errors.messages)
-      end
+      raise_error(@charge_transaction.errors.messages) unless @charge_transaction.save
     end
 
     def decrement_merchant_sum_of_payments
@@ -72,7 +64,6 @@ module PaymentCases
       merchant.total_transaction_sum -= @refund_transaction.amount
       raise_error(merchant.errors.messages) unless merchant.save
     end
-
 
     def approve_refund_transaction
       within_error_handler do
--- app/use_cases/payment_cases/refuse_payment.rb
+++ app/use_cases/payment_cases/refuse_payment.rb
@@ -1,60 +1,55 @@
 module PaymentCases
   class RefusePayment < PaymentCases::Base
-      def call
-        begin
-          load_authorize_transaction
-          AuthorizeTransaction.transaction do
-            ReversalTransaction.transaction do
-              create_reversal_transaction
-              reverse_authorize_transaction
-              approve_reversal_transaction
-            end
-          end
-        rescue UseCaseError => e
-          error_response(e.message)
-        else
-          success_response
+    def call
+      load_authorize_transaction
+      AuthorizeTransaction.transaction do
+        ReversalTransaction.transaction do
+          create_reversal_transaction
+          reverse_authorize_transaction
+          approve_reversal_transaction
         end
       end
-  
-      private
-  
-      attr_reader :options
-  
-      def load_authorize_transaction
-        @authorize_transaction = Transaction.where(
-          customer_email: options[:customer_email]
-          uuid: options[:uuid]
-        ).reorder(:created_at).last
-  
-        if @authorize_transaction.nil? ||
-          @authorize_transaction.is_a?(AuthorizeTransaction) ||
-          !@authorize_transaction.initial?
-          raise_error(:no_authorize_transaction)
-        end
-      end
+    rescue UseCaseError => e
+      error_response(e.message)
+    else
+      success_response
+    end
 
-      def create_reversal_transaction
-        @reversal_transaction = RefundTransaction.new(
-          uuid: options[:uuid],
-          customer_phone: options[:customer_phone],
-          customer_email: options[:customer_email]
-        )
-        unless @reversal_transaction.save
-          raise_error(@reversal_transaction.errors.messages)
-        end
-      end
-      
-      def reverse_authorize_transaction
-        within_error_handler do
-          @authorize_transaction.reverse!
-        end
-      end
+    private
 
-      def approve_reversal_transaction
-        within_error_handler do
-          @reversal_transaction.approve!
-        end
+    attr_reader :options
+
+    def load_authorize_transaction
+      @authorize_transaction = Transaction.where(
+        customer_email: options[:customer_email],
+        uuid: options[:uuid]
+      ).reorder(:created_at).last
+
+      if @authorize_transaction.nil? ||
+         !@authorize_transaction.is_a?(AuthorizeTransaction) ||
+         !@authorize_transaction.initial?
+        raise_error(:no_authorize_transaction)
+      end
+    end
+
+    def create_reversal_transaction
+      @reversal_transaction = RefundTransaction.new(
+        uuid: options[:uuid],
+        customer_phone: options[:customer_phone],
+        customer_email: options[:customer_email]
+      )
+      raise_error(@reversal_transaction.errors.messages) unless @reversal_transaction.save
+    end
+
+    def reverse_authorize_transaction
+      within_error_handler do
+        @authorize_transaction.reverse!
+      end
+    end
+
+    def approve_reversal_transaction
+      within_error_handler do
+        @reversal_transaction.approve!
       end
     end
   end
