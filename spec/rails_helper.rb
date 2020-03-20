bf42d0555262497e0c7683da86f96f67cf7ca300
Alexey Kuznetsov <m2lxkuz@gmail.com>
2020-03-20 22:09:43 +0500

    Polish rails helper with rubocop rules

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -71,6 +71,6 @@ RSpec.configure do |config|
   # config.filter_gems_from_backtrace("gem name")
 
   config.before(:each, type: :request) do
-    host! "localhost"
+    host! 'localhost'
   end
 end
